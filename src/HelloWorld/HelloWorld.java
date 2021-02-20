54da8c78bb734919cb084fa5be756d790d7ff240
StevieClark-super <78661590+StevieClark-super@users.noreply.github.com>
2021-02-20 15:20:13 -0500

    initial

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int HelloWorld = 6;
+        System.out.println(HelloWorld);
+    }
+}
---
+++ src/Variables/MyFirstVariable.java
@@ -0,0 +1,20 @@
+package Variables;
+
+public class MyFirstVariable {
+    public static void main(String[] args) {
+        int myFirstVaribable = 5;
+        System.out.println(myFirstVaribable);
+        int mySecondVaribable =10;
+        System.out.println(mySecondVaribable);
+        int myThirdvaribable = 5;
+        System.out.println(myThirdvaribable);
+        int myTotal=mySecondVaribable+myThirdvaribable;
+        System.out.println(myTotal);
+        int calculation=(mySecondVaribable+myThirdvaribable)*myTotal;
+        System.out.println(calculation);
+        int calcuation2=(mySecondVaribable+mySecondVaribable)*myFirstVaribable;
+        System.out.println(calcuation2);
+
+
+    }
+}
