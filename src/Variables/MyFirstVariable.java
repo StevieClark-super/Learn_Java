8cdf13343dfc809567131c38cb63eabb5ba36ff4
StevieClark-super <78661590+StevieClark-super@users.noreply.github.com>
2021-02-27 14:48:58 -0500

    long/doubles variables and conversions

--- src/Variables/MyFirstVariable.java
+++ src/Variables/MyFirstVariable.java
@@ -14,6 +14,58 @@ public class MyFirstVariable {
         System.out.println(calculation);
         int calcuation2=(mySecondVaribable+mySecondVaribable)*myFirstVaribable;
         System.out.println(calcuation2);
+        int minIntValue = Integer.MIN_VALUE;
+        System.out.println("Integer.MIN_VALUE =" + minIntValue);
+
+        int maxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer.MAX_VALUE =" + maxIntValue);
+
+        long minLongValue = Long.MIN_VALUE;
+
+        System.out.println("Long.MIN_VALUE =" +minLongValue);
+
+        long maxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long.Max_Value ="+maxLongValue);
+
+        long mylongvaribable = 100L;
+
+        System.out.println(mylongvaribable);
+
+        double someNumber = 3/2;
+
+        System.out.println(someNumber);
+
+        double someNumber2 = 3.0/2.0;
+
+        System.out.println(someNumber2);
+
+        int wrongType = (int) (3.0/2.0);
+
+        System.out.println(wrongType);
+
+        int celsius = 17;
+
+        System.out.println(celsius);
+
+        int fahrenheit = 81;
+
+        System.out.println(fahrenheit);
+
+        int conversion = (celsius + 32)/5*9;
+
+        System.out.println((double) conversion + "C");
+
+        int conversion2 = (fahrenheit/9*5)-32;
+
+        System.out.println((double) conversion2 + "F");
+
+        boolean isAgeAboveTen = true;
+
+        
+
+
 
 
     }
